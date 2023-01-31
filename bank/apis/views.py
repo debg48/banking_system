b9b6e684ecaa94a1bcf705e4ba6256aec66b334d
debg48 <debgandhar4000@gmail.com>
2023-01-31 12:35:00 +0530

    all apis done

--- bank/apis/views.py
+++ bank/apis/views.py
@@ -65,7 +65,6 @@ def create_account(request):
             new_acc['acc_no']=generate_account()
             try :
                 Account.objects.get(acc_no=new_acc['acc_no'])
-                gen = False
             except:
                 gen = True
                 break
@@ -87,6 +86,47 @@ def create_account(request):
             'message':'Invalid Name!',
             'success':False
         })
+
+# api to create multiple accounts
+
+@api_view(['POST'])
+def create_multi(request):
+    data = request.data
+    sucessfull=[]
+    unsucessfull=[]
+    for i in range(len(data)):
+        #check if name is valid or not
+        if (is_valid(str(data[str(i+1)]['name']))):
+            #create a new dictionary to store name
+            new_acc = {}
+            new_acc['name']=data[str(i+1)]['name']
+            # generae account no and check if smae is present in db
+            gen=False
+            while(gen==False):
+                new_acc['acc_no']=generate_account()
+                try :
+                    Account.objects.get(acc_no=new_acc['acc_no'])
+                except:
+                    gen = True
+                    break
+            if 'balance' in data[str(i+1)] and float(data[str(i+1)]['balance'])>0:    
+                new_acc['balance'] = data[str(i+1)]['balance']
+            else:
+                new_acc['balance']=0.00
+            account = Account()
+            account.name=new_acc['name']
+            account.acc_no=new_acc['acc_no']
+            account.balance=new_acc['balance']
+            account.save()
+            sucessfull.append(i+1)
+            
+        else:
+            unsucessfull.append(i+1)
+
+    return JsonResponse({
+                'sucessfull':sucessfull,
+                'unsuccessfull':unsucessfull
+            })
 
 # api to update name 
 
@@ -116,22 +156,23 @@ def update_name(request):
 @api_view(['POST'])
 def update_name_multi(request):
     data = request.data
+    sucessfull=[]
+    unsucessfull=[]
     for i in range(len(data)):
         if (is_valid(str(data[str(i+1)]['name']))):
             try :
                 account=Account.objects.get(acc_no=data[str(i+1)]['acc_no'])
+                sucessfull.append(i+1)
+                account.name= data[str(i+1)]['name']
+                account.save()
             except:
-                return JsonResponse({
-                    'message':'No match found !',
-                    'success':False
-                })
-            account.name= data[str(i+1)]['name']
-            account.save()
-            return JsonResponse(account.values())
+                unsucessfull.append(i+1)
+                        
         else:
-            return JsonResponse({
-                'message':'Invalid Name!',
-                'success':False
+            unsucessfull.append(i+1)
+    return JsonResponse({
+                'sucessfull':sucessfull,
+                'unsuccessfull':unsucessfull
             })
 
 
@@ -157,6 +198,30 @@ def deposit(request):
         account.save()
 
         return JsonResponse(account.values())
+
+# api for mutiple deposit 
+
+@api_view(['POST'])
+def deposit_multi(request):
+    data = request.data
+    sucessfull=[]
+    unsucessfull=[]
+    for i in range(len(data)):
+        if float(data[str(i+1)]['amount'])<=0.00 :
+            unsucessfull.append(i+1)
+        else:
+            try :
+                account=Account.objects.get(acc_no=data[str(i+1)]['acc_no'])
+                account.balance= float(account.balance) + float(data[str(i+1)]['amount'])
+                account.save()
+                sucessfull.append(i+1)
+            except:
+                unsucessfull.append(i+1)
+                 
+    return JsonResponse({
+                'sucessfull':sucessfull,
+                'unsuccessfull':unsucessfull
+            })
 
 # api to withdraw money
 
@@ -189,7 +254,29 @@ def debit(request):
             return JsonResponse(account.values())
         
         
-        
+# api to do multiple withdrawal
+
+@api_view(['POST'])
+def debit_multi(request):
+    data = request.data
+    sucessfull=[]
+    unsucessfull=[]
+    for i in range(len(data)):
+        if float(data[str(i+1)]['amount'])<=0.00 :
+            unsucessfull.append(i+1)
+        else:
+            try :
+                account=Account.objects.get(acc_no=data[str(i+1)]['acc_no'])
+                account.balance= float(account.balance) - float(data[str(i+1)]['amount'])
+                account.save()
+                sucessfull.append(i+1)
+            except:
+                unsucessfull.append(i+1)
+                 
+    return JsonResponse({
+                'sucessfull':sucessfull,
+                'unsuccessfull':unsucessfull
+            })
 
 # api to delete an account 
 
@@ -209,6 +296,27 @@ def delete_account(request):
             'message':'No match found !',
             'success':False
         })
+
+# api to delete multiple accounts
+@api_view(['POST'])
+def delete_acc_multi(request):
+    data = request.data
+    sucessfull=[]
+    unsucessfull=[]
+    for i in range(len(data)):
+        try:
+            account = Account.objects.get(acc_no=data[str(i+1)]['acc_no'])
+            account.delete()
+            sucessfull.append(i+1)
+        except:
+            unsucessfull.append(i+1)
+    
+    return JsonResponse({
+                'sucessfull':sucessfull,
+                'unsuccessfull':unsucessfull
+            })
+
+# api to delete all the accounts
 
 @api_view(['GET'])
 def del_all_account(request):
